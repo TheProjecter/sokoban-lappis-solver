e4d936f2bc17f3bc3996995fcc4929db997bbed0
carlos.a.colmenares.r <carlos.a.colmenares.r@6948df0f-d1e9-ebf5-ac21-cf99b44cff07>
2010-09-21 23:07:41 +0000

    I added two functions:
    - compute_area: mades a flod algorithm to calculate the area of the player in the node
    
    - precompute_neighbors: builds a matrix to have precalculated the nieghbor cells of every cell in the relative representation of the board

--- trunk/src/sokoban-lappis-solver.cpp
+++ trunk/src/sokoban-lappis-solver.cpp
@@ -17,6 +17,24 @@ int main(){
                         rel_to_abs_table, goals_pos,
                         init_node);
 
+    int board_height = board.size();
+
+    //Precompute the neighbors matrix
+    
+    int (*neighbors)[4], *num_neighbors;
+    
+    precompute_neighbors(
+                        board_height, board_width, num_cells,
+                        abs_to_rel_table, rel_to_abs_table,
+                        neighbors, num_neighbors
+                    );
+
+    //Make the auxiliary stack
+    int *stack_arr = new int[num_cells];
+
+    compute_area(   num_cells, neighbors,
+                    num_neighbors, stack_arr, init_node );
+
     return 0;
 }
 
@@ -50,7 +68,88 @@ int precompute_board(
 
 //...........CARLOS FUNCTIONs............
 
+void precompute_neighbors(
+                    int board_height, int board_width, int num_cells,
+                    int *abs_to_rel_table, int *rel_to_abs_table,
+                    int (*neighbors)[4], int *num_neighbors
+                ){
 
+    //Create the arrays
+    neighbors = new int[num_cells][4];
+
+    num_neighbors = new int[num_cells];
+    memset(num_neighbors, 0, sizeof(int) * num_cells);
+
+    const int move[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+    //For each cell in the relative representation,
+    //look for its neighbors in the absolute one
+    for(int i=0; i<num_cells; i++){
+        int abs_pos = rel_to_abs_table[i];
+        int y = abs_pos/board_width;
+        int x = abs_pos%board_width;
+
+        for(int j=0;j<4;j++){
+            int ny = y + move[j][0];
+            int nx = x + move[j][1];
+
+            if( 0<=ny && ny<board_height &&
+                0<=nx && nx<board_width &&
+                abs_to_rel_table[ny*board_width + nx] != -1
+                )
+                neighbors[i][ num_neighbors[i]++ ] =
+                    abs_to_rel_table[ny*board_width + nx];
+        }
+    }
+}
+
+void compute_area(  int num_cells, 
+                    int (*neighbors)[4], int *num_neighbors,
+                    int *stack_arr, Node *node ){
+
+    int arr_size = num_cells/32;
+    if( num_cells%32 !=0 ) arr_size++;
+
+    //find a valid position in the node to start with
+    int p1 = 0;
+    int p2 = 0;
+    int mask = 1;
+    while(true){
+        while( p2<32 && !( node->area[p1] & mask ) ){
+            p2++;
+            mask<<=1;
+        }
+        if(p2<32) break;
+        p1++;
+    }
+
+    //code for a DFS using a stack...
+
+    int st_sz = 0; //The stack's size
+
+    //push the first element
+    stack_arr[st_sz++]= p1*32 + p2;
+
+    while( st_sz != 0 ){
+
+        //Take the top element and pop
+        int current_cell = stack_arr[ --st_sz ];
+
+        for(int i=0; i<num_neighbors[current_cell]; i++){
+
+            int neig_cell = neighbors[current_cell][i];
+            p1 = neig_cell/32;
+            p2 = neig_cell%32;
+            mask = 1<<p2;
+
+            if( !( node->area[p1]&mask ) ){
+                node->area[p1] |= mask;
+                //push the element
+                stack_arr[st_sz++] = neig_cell;
+            }
+        }
+    }
+}
 
 //...........END CARLOS FUNCTIONs............
 
--- trunk/src/sokoban-lappis-solver.h
+++ trunk/src/sokoban-lappis-solver.h
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<vector>
 #include<string>
+#include<string.h>
 #include<iostream>
 using namespace std;
 
@@ -63,6 +64,60 @@ int precompute_board(
 
 //...........CARLOS FUNCTIONs............
 
+/**
+ * This function precomputes the neighbors of
+ * every cell in the relative representation of the
+ * problem. These neighbors will be used afterwards
+ * for computing areas.
+ *
+ * @param board_height      The height of the board.
+ *
+ * @param board_width       The width of the board.
+ *
+ * @param num_cells         The number of cells in the absolute
+ *                          representation of the board.
+ *
+ * @param abs_to_rel_table  Transformation table from absolute
+ *                          positions to relative ones.
+ *
+ * @param rel_to_abs_table  Transformation table from relative
+ *                          positions to absolute ones.
+ *
+ * @param neighbors         This pointer will end up pointing
+ *                          to the neighbors matrix.
+ *
+ * @param num_neighbors     This pointer will end up pointing
+ *                          to an array that contains the number
+ *                          of neighboring cells for each cell
+ *                          in the absolute representation of the
+ *                          board.
+ */
+void precompute_neighbors(
+                    int board_height, int board_width, int num_cells,
+                    int *abs_to_rel_table, int *rel_to_abs_table,
+                    int (*neighbors)[4], int *num_neighbors
+                );
+
+/**
+ * Given a node, makes a flood algorithm that calculates
+ * the active area that can be reached by the player.
+ *
+ * @param num_cells         The number of total reachable cells
+ *                          in the board.
+ *
+ * @param neighbors         A matrix with the neighboring cells
+ *                          of every cell in the relative representation
+ *
+ * @param num_neighbors     An array with the number of neighboring
+ *                          cells of each cell in the relative representation
+ *
+ * @param stack_arr         An array of size "num_cells" to be used
+ *                          as a stack.
+ *
+ * @param node              The node where to make the flood.
+ */
+void compute_area(  int num_cells, int (*neighbors)[4],
+                    int *num_neighbors, int *stack_arr, Node *node );
 
 
 //...........END CARLOS FUNCTIONs............
