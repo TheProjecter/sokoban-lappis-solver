03561a0a2826d57ecc69162748979a8441244972
kevin.anc <kevin.anc@6948df0f-d1e9-ebf5-ac21-cf99b44cff07>
2010-10-06 18:26:19 +0000

    Deadlocks started. Detect on the edges and besides the walls. not linked with the algo. we should create a function is_deadlock() to simplify the code later. code checked.

--- trunk/src/sokoban-lappis-solver.h
+++ trunk/src/sokoban-lappis-solver.h
@@ -125,6 +125,64 @@ void dfs(vector< string > &board, int *abs_to_rel_table,
 	 int *rel_to_abs_table, int *goals_pos, int *box_pos,
 	 const int moves[4][2], int, int, int, int &c);
 
+/* Determine which cell are deadlocks. Boxes should never be put
+ * on these cells.
+ *
+ * @param rel_to_abs_table  Translation table, same as usual.
+ *
+ * @param abs_to_rel_table  Translation table, same as usual.
+ *
+ * @param deadlock_list     List we will fill in with index of
+ *                          deadlock cells..
+ *
+ * @param neighbors         This pointer will end up pointing
+ *                          to the neighbors matrix.
+ *
+ * @param num_neighbors     This pointer will end up pointing
+ *                          to an array that contains the number
+ *                          of neighboring cells for each cell
+ *                          in the absolute representation of the
+ *                          board.
+ *
+ * @param goals             This pointer aims at the goals' position.
+ *
+ * @param num_cell          Total amount of cells
+ *
+ * @param board_width       Size of the board.
+ *
+ */
+void init_deadlock_list(int *rel_to_abs_table, int *abs_to_rel_table,
+			int *deadlock_list, int (*neighbors)[4], 
+			int *&num_neighbors, int *goals,
+			int num_cell, int board_width);
+
+/* Function used by the deadlock search at the beginning.
+ * Some cells are against walls and so can be deadlocks.
+ * unless there is a goal on the line.
+ *
+ * @param rel_to_abs_table  Translation table, same as usual.
+ *
+ * @param deadlock_list     Deadlock cells
+ *
+ * @param goals             Goals' positions
+ *
+ * @param direction         Allow us to evaluate where are the neighbors
+ *
+ * @param dir               Which direction are we going ?
+ *
+ * @param from_cell         Cell we come from
+ *
+ * @param board_width       Size of the board.
+ *
+ */
+bool init_deadlock_beside_wall(int *rel_to_abs_table,
+			       int *abs_to_rel_table,
+			       int *deadlock_list, 
+			       int *goals,
+			       int direction[2][2], int dir,
+			       int from_cell, int board_width);
+
+
 inline void add_to_list(int* list, int index);
     
 /**
